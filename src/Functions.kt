d024099dcc86330ae25d452285fde7dd8502090c
gm-sourav <souravmahbub13@gmail.com>
2026-06-13 10:23:33 +0600

    First

--- src/Functions.kt
+++ src/Functions.kt
@@ -8,7 +8,14 @@ fun order(item: String, quantity: Int) {
     println("Your order is $item $quantity")
 }
 
+
+fun calculatePer(marks: Int, totalMarks:Int) : Float {
+    return (marks.toFloat() / totalMarks) * 100
+}
+
 fun main() {
     //calculateMarks(90, 70, 78)
-    order("Coffe", 5)
+   //order("Coffe", 5)
+    val result = calculatePer(90,100)
+    println("Result: $result%")
 }
