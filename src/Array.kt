3059c2936c8a18e640eb11b890d7983cbc0cdd1d
gm-sourav <souravmahbub13@gmail.com>
2026-06-13 15:43:23 +0600

    First

---
+++ src/Array.kt
@@ -0,0 +1,19 @@
+fun main() {
+//    var marks = arrayOf(85,90,89,40,70)
+
+
+//    println("Student 1 marks : ${marks[0]}")
+//    println("Student 2 marks : ${marks[1]}")
+//    println("Student 3 marks : ${marks[2]}")
+//    println("Student 4 marks : ${marks[3]}")
+//    println("Student 5 marks : ${marks[4]}")
+
+    var menu = arrayOf("Pizza", "Pepe", "Burger","Cold Drinks")
+    println("Todays Menu : ")
+
+    for (i in menu) {
+        println(i)
+    }
+
+
+}
